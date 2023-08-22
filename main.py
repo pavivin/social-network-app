41d5240257c38e05c6271fd70b5eb9ca382c04de
pavivin <pavivin@yandex.ru>
2023-08-22 16:40:27 +0700

    real status code

--- main.py
+++ main.py
@@ -84,7 +84,8 @@ async def uvicorn_base_exception_handler(request: Request, exc: Exception):
             message=error.message,
             body=error.payload,
             exception_class=error.__class__.__name__,
-        ).dict()
+        ).dict(),
+        status_code=error.status_code,
     )
 
 
@@ -98,7 +99,8 @@ async def unicorn_api_exception_handler(request: Request, exc: exceptions.ApiExc
             message=exc.message,
             body=exc.payload,
             exception_class=exc._type(),
-        ).dict()
+        ).dict(),
+        status_code=exc.status_code,
     )
 
 
@@ -112,7 +114,8 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
             message=error.message,
             body=error.payload,
             exception_class=error._type(),
-        ).dict()
+        ).dict(),
+        status_code=error.status_code,
     )
 
 
@@ -126,7 +129,8 @@ async def validation_http_exception_handler(request: Request, exc: HTTPException
             message=error.message,
             body=error.payload,
             exception_class=error._type(),
-        ).dict()
+        ).dict(),
+        status_code=error.status_code,
     )
 
 
