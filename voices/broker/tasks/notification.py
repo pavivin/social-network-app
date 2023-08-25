34b71efcd61fa12cc990ffdfceb35af041d59af9
pavivin <pavivin@yandex.ru>
2023-08-25 18:09:06 +0700

    mr proper

--- voices/broker/tasks/notification.py
+++ voices/broker/tasks/notification.py
@@ -1,95 +1,50 @@
-import random
 from datetime import datetime, timezone
+from enum import StrEnum
 from typing import List
-from enum import Enum
 
 from firebase_admin import messaging
 
-from voices.app.auth.models import Notification, User
+from voices.app.auth.models import User
+from voices.app.notifications.models import Notification
 from voices.broker import app, app_firebase, loop
 from voices.db.connection import Transaction
 
-class EventName(str, Enum):
-    like = "like"
-    request = "request_frends"
-    accept = "accept_frends"
-    share = "share"
-    comment = "comment"
-    answered = "answered"
-    change_status = "change_status"
+
+class EventName(StrEnum):
+    LIKE = "like"
+    REQUEST_FRENDS = "request_frends"
+    ACCEPT_FRENDS = "accept_frends"
+    SHARE = "share"
+    COMMENT = "comment"
+    ANSWERED = "answered"
+    CHANGE_STATUS = "change_status"
 
 
-async def firebase_notifications(tokens: List[str], user_id_send: str, user_id_get: str, title: str,status : EventName):
+status_text = {
+    EventName.LIKE: "Оценил вашу запись",
+    EventName.REQUEST_FRENDS: "Хочет добавить вас в друзья",
+    EventName.ACCEPT_FRENDS: "Принял вашу заявку",
+    EventName.SHARE: "Поделился вашей записью",
+    EventName.COMMENT: "Оставил комментарий под вашей записью",
+    EventName.ANSWERED: "Ответил вам",
+    EventName.CHANGE_STATUS: "Изменился статус интересовавшей вас проблемы",
+}
+
+
+async def firebase_notifications(tokens: List[str], user_id_send: str, user_id_get: str, status: EventName):
     async with Transaction():
         user: User = await User.get_by_id(user_id_send)
-        first_name, last_name = user.first_name or "user", user.last_name or random.randint(a=0, b=99999)
-        match status:
-            case "like":
-                data_send = {
-                    "text": "Оценил вашу запись",
-                    "picture": f"{user.picture_url}",
-                    "time": str(datetime.now(tz=timezone.utc)),
-                    "first_name": first_name,
-                    "last_name": f"{last_name}",
-                    "type":  status,
-                }
-            case "request_frends":
-                data_send = {
-                    "text": "Хочет добавить вас в друзья ",
-                    "picture": f"{user.picture_url}",
-                    "time": str(datetime.now(tz=timezone.utc)),
-                    "first_name": first_name,
-                    "last_name": f"{last_name}",
-                    "type":  status,
-                }
-            case "accept_frends":
-                data_send = {
-                    "text": "Принял вашу заявку",
-                    "picture": f"{user.picture_url}",
-                    "time": str(datetime.now(tz=timezone.utc)),
-                    "first_name": first_name,
-                    "last_name": f"{last_name}",
-                    "type":  status,
-                }
-            case "share":
-                data_send = {
-                    "text": "Поделился вашей записью",
-                    "picture": f"{user.picture_url}",
-                    "time": str(datetime.now(tz=timezone.utc)),
-                    "first_name": first_name,
-                    "last_name": f"{last_name}",
-                    "type":  status,
-                }
-            case "comment":
-                data_send = {
-                    "text": "Оставил комментарий под вашей записью",
-                    "picture": f"{user.picture_url}",
-                    "time": str(datetime.now(tz=timezone.utc)),
-                    "first_name": first_name,
-                    "last_name": f"{last_name}",
-                    "type":  status,
-                }
-            case "answered":
-                data_send = {
-                    "text": "Ответил вам",
-                    "picture": f"{user.picture_url}",
-                    "time": str(datetime.now(tz=timezone.utc)),
-                    "first_name": first_name,
-                    "last_name": f"{last_name}",
-                    "type":  status,
-                }
-            case "change_status":
-                data_send = {
-                    "text": "Изменился статус интересовавшей вас проблемы",
-                    "picture": f"{user.picture_url}",
-                    "time": str(datetime.now(tz=timezone.utc)),
-                    "first_name": first_name,
-                    "last_name": f"{last_name}",
-                    "type":  status,
-                }
-            case _:
-                return "Something's wrong with the status Notification"
-        await Notification.create(user_id_get, str(data_send["text"]), user.picture_url, first_name, f"{last_name}",status.title)
+        data_send = {
+            "text": status_text[status],
+            "picture": user.image_url,
+            "time": str(datetime.now(tz=timezone.utc)),
+            "first_name": user.first_name,
+            "last_name": user.last_name,
+            "type": status,
+        }
+        await Notification.create(
+            user_id_get, status_text[status], user.image_url, user.first_name, user.last_name, status
+        )
         for token in tokens:
             message = messaging.Message(
                 data=data_send,
@@ -100,5 +55,5 @@ async def firebase_notifications(tokens: List[str], user_id_send: str, user_id_g
 
 
 @app.task(name="send_notification")
-def send_notification(tokens: List[str], user_id_send: str, user_id_get: str, title: str, time_limit=70) -> int:
-    return loop.run_until_complete(firebase_notifications(tokens, user_id_send, user_id_get, title))
+def send_notification(tokens: List[str], user_id_send: str, user_id_get: str, status: str) -> int:
+    return loop.run_until_complete(firebase_notifications(tokens, user_id_send, user_id_get, status))
