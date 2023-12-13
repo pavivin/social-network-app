5c85dcab02c59dbb48aa579d9f5da68f847c3ef2
pavivin <pavivin@yandex.ru>
2023-12-13 20:47:23 +0700

    fix survey

--- voices/app/initiatives/controllers.py
+++ voices/app/initiatives/controllers.py
@@ -537,7 +537,7 @@ async def vote_initiative(initiative_id: uuid.UUID, body: SurveyVoteView, token:
                 if option.value is not None:
                     survey.blocks[i].answer[j].vote_count += 1
                     survey.blocks[i].answer[j].vote_percent = int(
-                        ((survey.blocks[i].answer[j].vote_count + 1) / survey.vote_count) * 100
+                        ((survey.blocks[i].answer[j].vote_count) / survey.vote_count) * 100
                     )
             except KeyError:
                 raise ValidationError(message="Not enough values in answer")
